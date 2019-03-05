b9993233ce1b0b9865eaa9b57f9e333a59dd62e5
Sean Beard <sean61793@my.uri.edu>
2019-03-04 19:54:04 -0500

    chore(LoginButton): add margin

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,14 +1,23 @@
 import * as React from "react";
+import styled from "styled-components";
 
 import { googleLogin } from "../utils/api";
 import { Button } from "./Button";
+
+const Wrapper = styled.div`
+  margin: 1rem;
+`;
 
 interface Props {
   onLoginSuccess: () => void;
 }
 
 const LoginButton: React.SFC<Props> = ({ onLoginSuccess }) => (
-  <Button onClick={() => googleLogin(onLoginSuccess)}>Sign In To Google</Button>
+  <Wrapper>
+    <Button onClick={() => googleLogin(onLoginSuccess)}>
+      Sign In To Google
+    </Button>
+  </Wrapper>
 );
 
 export default LoginButton;
