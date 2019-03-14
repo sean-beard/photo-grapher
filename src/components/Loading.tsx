956892c85b9c60972f1da0950323f6f251f5203c
Sean Beard <sean61793@my.uri.edu>
2019-03-13 20:15:50 -0400

    refactor(Loading): use Colors enum

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { PacmanLoader } from "react-spinners";
+import { Colors } from "../styles/Base";
 
-const Loader = () => <PacmanLoader color="#61dafb" />;
+const Loader = () => <PacmanLoader color={Colors.ACTION_BLUE} />;
 
 export default Loader;
