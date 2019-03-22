7665a8a0437644614dde61dcc2094b39fd31a6b1
Sean Beard <sean61793@my.uri.edu>
2019-03-22 15:16:43 -0400

    refactor(folders): convert to hooks implementation

--- src/components/Folders/index.tsx
+++ src/components/Folders/index.tsx
@@ -23,105 +23,92 @@ interface State {
   selectedFolderId: string;
 }
 
-class Folders extends React.Component<Props, State> {
-  state: State = {
-    folders: [],
-    loadingFolders: false,
-    loadingPhotos: false,
-    photos: [],
-    selectedFolderId: ""
+const Folders: React.FunctionComponent<Props> = ({
+  authorized,
+  onPhotoFetchSuccess,
+  onPhotoFetchFailure
+}) => {
+  if (!authorized) {
+    return null;
+  }
+
+  const [folders, setFolders] = React.useState<State["folders"]>([]);
+  const [loadingFolders, setLoadingFolders] = React.useState<
+    State["loadingFolders"]
+  >(false);
+  const [loadingPhotos, setLoadingPhotos] = React.useState<
+    State["loadingPhotos"]
+  >(false);
+  const [photos, setPhotos] = React.useState<State["photos"]>([]);
+  const [selectedFolderId, setSelectedFolderId] = React.useState<
+    State["selectedFolderId"]
+  >("");
+
+  React.useEffect(() => {
+    if (authorized) {
+      getFolders();
+    }
+  }, [authorized]);
+
+  const getFolders = () => {
+    setLoadingFolders(true);
+    fetchRootLevelDriveFolders(
+      handleFolderFetchSuccess,
+      handleFolderFetchError
+    );
   };
 
-  componentDidMount() {
-    const { authorized } = this.props;
-    if (authorized) {
-      this.getFolders();
-    }
-  }
-
-  componentDidUpdate(prevProps: Props) {
-    if (!prevProps.authorized && this.props.authorized) {
-      this.getFolders();
-    }
-  }
-
-  getFolders = () =>
-    this.setState({ loadingFolders: true }, () =>
-      fetchRootLevelDriveFolders(
-        this.handleFolderFetchSuccess,
-        this.handleFolderFetchError
-      )
-    );
-
-  handleFolderFetchSuccess = (
+  const handleFolderFetchSuccess = (
     response: gapi.client.HttpRequestFulfilled<any>
-  ) => this.setState({ folders: response.result.files, loadingFolders: false });
+  ) => {
+    setLoadingFolders(false);
+    setFolders(response.result.files);
+  };
 
-  handleFolderFetchError = (error: any) =>
-    this.setState({ loadingFolders: false }, () =>
-      console.log(`Error fetching folders: ${error.message}`)
+  const handleFolderFetchError = (error: any) => {
+    console.log(`Error fetching folders: ${error.message}`);
+    setLoadingFolders(false);
+  };
+
+  const handleFolderSelection = (folderId: string) => {
+    setLoadingPhotos(true);
+    fetchDrivePhotosWithLocFromFolder(
+      folderId,
+      response => handlePhotosFetchSuccess(folderId, response),
+      handlePhotosFetchError
     );
+  };
 
-  handleFolderSelection = (folderId: string) =>
-    this.setState({ loadingPhotos: true }, () =>
-      fetchDrivePhotosWithLocFromFolder(
-        folderId,
-        response => this.handlePhotosFetchSuccess(folderId, response),
-        this.handlePhotosFetchError
-      )
-    );
-
-  handlePhotosFetchSuccess = (
+  const handlePhotosFetchSuccess = (
     folderId: string,
     response: gapi.client.HttpRequestFulfilled<any>
   ) => {
     const photos = getPhotosWithLocation(response.result.files);
-    this.setState(
-      {
-        loadingPhotos: false,
-        photos,
-        selectedFolderId: folderId
-      },
-      () => {
-        if (photos.length > 0) {
-          this.props.onPhotoFetchSuccess(photos);
-        } else {
-          this.props.onPhotoFetchFailure();
-        }
-      }
-    );
+    setLoadingPhotos(false);
+    setPhotos(photos);
+    setSelectedFolderId(folderId);
+    if (photos.length > 0) {
+      onPhotoFetchSuccess(photos);
+    } else {
+      onPhotoFetchFailure();
+    }
   };
 
-  handlePhotosFetchError = (error: any) =>
-    this.setState({ loadingPhotos: false }, () => {
-      console.log(`Error fetching photos: ${error.message}`);
-      this.props.onPhotoFetchFailure();
-    });
+  const handlePhotosFetchError = (error: any) => {
+    console.log(`Error fetching photos: ${error.message}`);
+    setLoadingPhotos(false);
+    onPhotoFetchFailure();
+  };
 
-  render() {
-    if (!this.props.authorized) {
-      return null;
-    }
-
-    const {
-      folders,
-      loadingFolders,
-      loadingPhotos,
-      photos,
-      selectedFolderId
-    } = this.state;
-    const photosExist = photos.length > 0;
-
-    return (
-      <>
-        {selectedFolderId && !photosExist && (
-          <h2>Whoops... Couldn't find any photos with location data.</h2>
-        )}
-        <FolderList {...{ folders }} onSelection={this.handleFolderSelection} />
-        {(loadingFolders || loadingPhotos) && <Loader />}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {selectedFolderId && photos.length < 1 && (
+        <h2>Whoops... Couldn't find any photos with location data.</h2>
+      )}
+      <FolderList {...{ folders }} onSelection={handleFolderSelection} />
+      {(loadingFolders || loadingPhotos) && <Loader />}
+    </>
+  );
+};
 
 export default Folders;
