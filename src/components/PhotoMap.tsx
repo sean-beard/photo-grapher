4e7007a8bea07e79e7c489c196fdad5a11234a15
Sean Beard <sean61793@my.uri.edu>
2019-05-10 20:14:24 -0400

    refactor(PhotoMap): early return if there are no photos

--- src/components/PhotoMap.tsx
+++ src/components/PhotoMap.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
 import styled from "styled-components";
 import { Map as LMap, TileLayer, Marker } from "react-leaflet";
-import { path } from "ramda";
+import { path, isEmpty } from "ramda";
 
 import { Photo } from "types/api";
 import ImagePopup from "components/ImagePopup";
 import { Spacing } from "styles/Base";
-import { hasItems } from "utils/data-operations";
 import { getLatLongCenter } from "utils/map";
 import { getLocations } from "utils/photos";
 
@@ -23,7 +22,7 @@ interface Props {
 
 const PlotPhotos: React.FunctionComponent<Props> = ({ photos }) => (
   <>
-    {(photos || []).map(({ id, imageMediaMetadata: { location } }) => {
+    {photos.map(({ id, imageMediaMetadata: { location } }) => {
       const lat: number | undefined = path(["latitude"], location || {});
       const long: number | undefined = path(["longitude"], location || {});
 
@@ -40,14 +39,18 @@ const PlotPhotos: React.FunctionComponent<Props> = ({ photos }) => (
 );
 
 const PhotoMap: React.FunctionComponent<Props> = ({ photos }) => {
+  if (isEmpty(photos)) {
+    return null;
+  }
+
   // TODO: dynamic zoom value
   const center = getLatLongCenter(getLocations(photos));
-  return hasItems(photos) ? (
+  return (
     <LeafletMap {...{ center }} zoom={8}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <PlotPhotos {...{ photos }} />
     </LeafletMap>
-  ) : null;
+  );
 };
 
 export default PhotoMap;
