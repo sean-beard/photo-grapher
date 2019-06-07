c32d7084fc1e68ee9602f7b83066612861f1b007
Sean Beard <sean61793@my.uri.edu>
2019-06-07 13:40:24 -0400

    ui(Scrollable): default to full width

--- src/components/Scrollable.tsx
+++ src/components/Scrollable.tsx
@@ -8,15 +8,17 @@ import {
 } from "victory";
 
 const Scrollable: React.FC = ({ children }) => (
-  <svg
-    viewBox="0 0 350 350"
-    width="350"
-    height="350"
-    role="image"
-    aria-label="victory-chart"
-  >
-    {children}
-  </svg>
+  <div style={{ width: "100%" }}>
+    <svg
+      viewBox="0 0 350 350"
+      width="auto"
+      height="auto"
+      role="image"
+      aria-label="victory-chart"
+    >
+      {children}
+    </svg>
+  </div>
 );
 
 export const ScrollableChart: React.FC<VictoryChartProps> = ({
